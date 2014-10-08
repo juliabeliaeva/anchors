73cb3ce87b3155819f73a645622ab4bf968fb930
Julia Beliaeva <julia.beliaeva@jetbrains.com>
2014-10-08 06:20:34 +0400

    date now also works on linux

--- date.sh
+++ date.sh
@@ -2,6 +2,11 @@
 
 ad=`git log -1 --date=short --format="format:%ad"`
 #echo ${ad}
-ru_ad=`LANG=ru_RU date -jf "%Y-%m-%d" ${ad} "+%e %B %Y"`
+if [ `uname` = "Linux" ]; then
+	ru_ad=`LC_ALL=ru_RU.utf8 date -d ${ad} "+%e %B %Y"`
+else
+	# I'm gonna cry here
+	ru_ad=`LANG=ru_RU date -jf "%Y-%m-%d" ${ad} "+%e %B %Y"`
+fi
 #echo ${ru_ad}
 echo "\\gdef\\GITAuthorDate{"${ru_ad}"}"
